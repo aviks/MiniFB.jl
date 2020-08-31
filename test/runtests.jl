832cce027f43617417b17d1583d10c4262585cc3
Avik Sengupta <avik@sengupta.net>
2020-08-31 13:18:36 +0100

    fix tests and examples

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
     #    since the windows needs to be closed manually
     #    Best we do is to test the package loads. 
     #    Look at examples directory for usage hints. 
-    @test 1+1
+    @test 1+1 == 2
 end
 
 
